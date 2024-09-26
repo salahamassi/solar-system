2c3ebcd14148d966aeeba232bea94a57576c4e1d
yousifmedhat98 <y.medhat@bimventures.com>
2024-09-26 11:23:44 +0300

    Refactor SolarSection component layout and styling

--- app/components/SolarSection.tsx
+++ app/components/SolarSection.tsx
@@ -1,37 +1,44 @@
-import Image from 'next/image';
-import { Button } from '@mui/material';
+import Image from "next/image";
+import { Button } from "@mui/material";
+import Link from "next/link";
 
 export default function SolarSection(): JSX.Element {
   return (
-    <section className="flex justify-center bg-white space-x-8">
-      
-      <div className='w-1/4'>
-        <h2 className="text-gray-700 text-2xl">
-        Energy Freedom with Solar and Storage
+    <section className="container max-w-4xl mx-auto pt-12 pb-72">
+      <div className="flex gap-5">
+        <h2 className="w-[220px] text-gray-700 text-4xl">
+          Energy Freedom with Solar and Storage
         </h2>
-      </div>
-
-      <div className="w-[6px] bg-orange-500 h-auto"></div>
-
-      <div className="flex flex-col w-1/3 space-x-8">
-        <p className="text-gray-700 text-lg mb-6">
-        Vulputate dignissim suspendisse in est ante in nibh mauris. Neque aliquam vestibulum morbi blandit cursus risus. Nisi est sit amet facilisis. Volutpat commodo sed egestas egestas. Id leo in vitae turpis massa sed elementum tempus. Non curabitur gravida arcu ac tortor dignissim convallis aenean.
-        </p>
-        <p className="text-gray-700">Image by
-            <a href="https://www.freepik.com/photos/eco" className="text-gray-700" target="_blank">Freepik</a>
-         </p>
-        <Button variant="contained" color="primary">Learn More</Button>
-      </div>
-
-      <div className="w-1/4">
+        <div className="w-[10px] bg-orange-500 h-[26rem]" />
+        <div className=" space-y-5">
+          <p className="text-gray-500 text-sm w-60">
+            Vulputate dignissim suspendisse in est ante in nibh mauris. Neque
+            aliquam vestibulum morbi blandit cursus risus. Nisi est sit amet
+            facilisis. Volutpat commodo sed egestas egestas. Id leo in vitae
+            turpis massa sed elementum tempus. Non curabitur gravida arcu ac
+            tortor dignissim convallis aenean.
+          </p>
+          <p className="text-gray-500 text-sm">
+            {"Image by" + " "}
+            <Link
+              href="https://www.freepik.com/photos/eco"
+              className="text-gray-700 underline"
+              target="_blank"
+            >
+              Freepik
+            </Link>
+          </p>
+          <Button variant="contained" color="primary">
+            Learn More
+          </Button>
+        </div>
         <Image
           src="/solar-house.png"
           alt="Solar powered house"
-          width={500}
-          height={400}
+          width={300}
+          height={900}
         />
       </div>
-      
     </section>
   );
 }
